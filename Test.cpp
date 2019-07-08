3606e479f2f7e2776df34e1a4c2c1e115e8df93d
Pawan Kumar <pawan.kumar@Stickerno-196.local>
2019-07-08 15:47:01 +0530

    first commit

---
+++ Test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Pawan Kumar on 2019-07-07.
+//
+
+#include "Test.h"
+
+int incrementData(int data) {
+    return data++;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Test.cpp"
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << incrementData(10) << std::endl;
+    return 0;
+}
