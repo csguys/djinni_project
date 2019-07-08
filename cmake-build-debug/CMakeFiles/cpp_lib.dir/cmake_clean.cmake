ce4753e7186b1cf529b3af9485304db8ee348691
Pawan Kumar <pawan.kumar@Stickerno-196.local>
2019-07-08 18:09:29 +0530

    This reverts commit 278288f40a2a6181bfd8e505bb4a47f5df867c8e.

---
+++ cmake-build-debug/CMakeFiles/cpp_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_lib.dir/main.cpp.o"
+  "cpp_lib.pdb"
+  "cpp_lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
